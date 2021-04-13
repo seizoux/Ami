c33836f3e1e5fbfaec2cf59c83482cdcda551f06
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-04-13 15:21:57 -0700

    Update covid19.py

--- cogs/covid19.py
+++ cogs/covid19.py
@@ -24,36 +24,36 @@ class Covid(commands.Cog):
 
             else:
                 url = f"https://coronavirus-19-api.herokuapp.com/countries/{countryName}"
-                stats = requests.get(url)
-                json_stats = stats.json()
-                country = json_stats["country"]
-                totalCases = json_stats["cases"]
-                todayCases = json_stats["todayCases"]
-                totalDeaths = json_stats["deaths"]
-                todayDeaths = json_stats["todayDeaths"]
-                recovered = json_stats["recovered"]
-                active = json_stats["active"]
-                critical = json_stats["critical"]
-                casesPerOneMillion = json_stats["casesPerOneMillion"]
-                deathsPerOneMillion = json_stats["deathsPerOneMillion"]
-                totalTests = json_stats["totalTests"]
-                testsPerOneMillion = json_stats["testsPerOneMillion"]
+                async with self.bot.session.get(url) as stats:
+                    json_stats = await stats.json()
+                    country = json_stats["country"]
+                    totalCases = json_stats["cases"]
+                    todayCases = json_stats["todayCases"]
+                    totalDeaths = json_stats["deaths"]
+                    todayDeaths = json_stats["todayDeaths"]
+                    recovered = json_stats["recovered"]
+                    active = json_stats["active"]
+                    critical = json_stats["critical"]
+                    casesPerOneMillion = json_stats["casesPerOneMillion"]
+                    deathsPerOneMillion = json_stats["deathsPerOneMillion"]
+                    totalTests = json_stats["totalTests"]
+                    testsPerOneMillion = json_stats["testsPerOneMillion"]
 
-                embed2 = discord.Embed(title=f"**COVID-19 Status Of {country}**!", colour=0xffcff1, timestamp=ctx.message.created_at)
-                embed2.add_field(name="**Total Cases**", value=totalCases, inline=True)
-                embed2.add_field(name="**Today Cases**", value=todayCases, inline=True)
-                embed2.add_field(name="**Total Deaths**", value=totalDeaths, inline=True)
-                embed2.add_field(name="**Today Deaths**", value=todayDeaths, inline=True)
-                embed2.add_field(name="**Recovered**", value=recovered, inline=True)
-                embed2.add_field(name="**Active**", value=active, inline=True)
-                embed2.add_field(name="**Critical**", value=critical, inline=True)
-                embed2.add_field(name="**Cases Per One Million**", value=casesPerOneMillion, inline=True)
-                embed2.add_field(name="**Deaths Per One Million**", value=deathsPerOneMillion, inline=True)
-                embed2.add_field(name="**Total Tests**", value=totalTests, inline=True)
-                embed2.add_field(name="**Tests Per One Million**", value=testsPerOneMillion, inline=True)
+                    embed2 = discord.Embed(title=f"**COVID-19 Status Of {country}**!", colour=0xffcff1, timestamp=ctx.message.created_at)
+                    embed2.add_field(name="**Total Cases**", value=totalCases, inline=True)
+                    embed2.add_field(name="**Today Cases**", value=todayCases, inline=True)
+                    embed2.add_field(name="**Total Deaths**", value=totalDeaths, inline=True)
+                    embed2.add_field(name="**Today Deaths**", value=todayDeaths, inline=True)
+                    embed2.add_field(name="**Recovered**", value=recovered, inline=True)
+                    embed2.add_field(name="**Active**", value=active, inline=True)
+                    embed2.add_field(name="**Critical**", value=critical, inline=True)
+                    embed2.add_field(name="**Cases Per One Million**", value=casesPerOneMillion, inline=True)
+                    embed2.add_field(name="**Deaths Per One Million**", value=deathsPerOneMillion, inline=True)
+                    embed2.add_field(name="**Total Tests**", value=totalTests, inline=True)
+                    embed2.add_field(name="**Tests Per One Million**", value=testsPerOneMillion, inline=True)
 
-                embed2.set_thumbnail(url="https://cdn.discordapp.com/attachments/564520348821749766/701422183217365052/2Q.png")
-                await ctx.send(embed=embed2)
+                    embed2.set_thumbnail(url="https://cdn.discordapp.com/attachments/564520348821749766/701422183217365052/2Q.png")
+                    await ctx.send(embed=embed2)
 
         except:
             embed3 = discord.Embed(title="Invalid Country Name!", colour=0xffcff1, timestamp=ctx.message.created_at)
