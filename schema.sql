e7dae577122c5e1aaed60090f8803fb0bb0dacb5
Jadon <47833807+WhoTheOOF@users.noreply.github.com>
2021-05-07 16:36:28 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,8 +1,20 @@
-CREATE TABLE IF NOT EXISTS blacklist (
+CREATE TABLE public.antispam (
+    guild_id character varying,
+    toggle text
+);
+
+
+ALTER TABLE public.antispam OWNER TO postgres;
+
+CREATE TABLE public.blacklist (
     user_id character varying NOT NULL,
     reason text
 );
-CREATE TABLE IF NOT EXISTS items (
+
+
+ALTER TABLE public.blacklist OWNER TO postgres;
+
+CREATE TABLE public.items (
     user_id character varying,
     water_bottles bigint DEFAULT 0 NOT NULL,
     redrums bigint DEFAULT 0 NOT NULL,
@@ -15,30 +27,51 @@ CREATE TABLE IF NOT EXISTS items (
     rare_chests bigint DEFAULT 0 NOT NULL,
     epic_chests bigint DEFAULT 0 NOT NULL
 );
-CREATE TABLE IF NOT EXISTS levels (
+
+
+ALTER TABLE public.items OWNER TO postgres;
+
+CREATE TABLE public.levels (
     guild_id character varying,
     user_id character varying,
     level bigint,
     xp bigint,
     level_bg text
 );
-CREATE TABLE IF NOT EXISTS numbers (
+
+
+ALTER TABLE public.levels OWNER TO postgres;
+
+CREATE TABLE public.numbers (
     user_id character varying,
     hugs bigint DEFAULT 0 NOT NULL,
     slaps bigint DEFAULT 0 NOT NULL,
     kills bigint DEFAULT 0 NOT NULL,
     kisses bigint DEFAULT 0 NOT NULL,
-    licks bigint DEFAULT 0 NOT NULL
+    licks bigint DEFAULT 0 NOT NULL,
+    punches bigint DEFAULT 0 NOT NULL,
+    pats bigint DEFAULT 0 NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS premium (
+
+ALTER TABLE public.numbers OWNER TO postgres;
+
+CREATE TABLE public.premium (
     user_id character varying,
     sub_date text
 );
-CREATE TABLE IF NOT EXISTS testers (
+
+
+ALTER TABLE public.premium OWNER TO postgres;
+
+CREATE TABLE public.testers (
     id character varying NOT NULL
 );
-CREATE TABLE IF NOT EXISTS users (
+
+
+ALTER TABLE public.testers OWNER TO postgres;
+
+CREATE TABLE public.users (
     user_id character varying,
     wallet bigint,
     bank bigint,
@@ -48,7 +81,11 @@ CREATE TABLE IF NOT EXISTS users (
     investments bigint DEFAULT 0 NOT NULL,
     total_earn bigint DEFAULT 0 NOT NULL
 );
-CREATE TABLE IF NOT EXISTS welcome (
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+CREATE TABLE public.welcome (
     guild_id character varying NOT NULL,
     msg text,
     role_name character varying,
@@ -57,3 +94,6 @@ CREATE TABLE IF NOT EXISTS welcome (
     embed character varying,
     welc character varying
 );
+
+
+ALTER TABLE public.welcome OWNER TO postgres;
