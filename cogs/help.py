31102a0e6fe8b216d4c1749545e14d25bbb5e32a
Daishiky <77997662+Daishiky@users.noreply.github.com>
2021-05-21 00:55:22 +0200

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands.errors import CommandNotFound
+from asyncdagpi import Client, ImageFeatures
+
+dagpi = Client("t35iaK4gi36TH3N2q0zd95A1h9m4TeInrLP6oyibSaK97q9SCRggzMUlCMWLkklU")
 
 class Help(commands.Cog):
     def __init__(self, bot):
@@ -14,12 +17,56 @@ class Help(commands.Cog):
 
     @commands.command(help="Show this message.")
     async def help(self, ctx, command=None):
+        if command == "moderation":
+            embed=discord.Embed(title="Moderation commands", description=f"• `ban`, `kick`, `mute`, `muterole`, `unban`, `unmute`, `cleanup`, `slowmode`, `antispam`", color=0xffcff1)
+            embed.set_author(name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}")
+            return await ctx.send(embed=embed)
+        elif command == "economy":
+            embed=discord.Embed(title="Economy commands", description=f"• `bal`, `dep`, `wd`, `shop`, `buyitem`, `garage`, `drink`, `hunt`, `smoke`, `fish`, `bankrob`, `openchest`, `oc`, `rob`, `send`, `daily`, `mine`, `invest`, `petshop`, `buypet`, `petname`, `lb`, `glbc`", color=0xffcff1)
+            embed.set_author(name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}")
+            return await ctx.send(embed=embed)
+        elif command == "music":
+            embed=discord.Embed(title="Music commands", description=f"• `play`, `join`, `leave`, `volume`, `stop`, `pause`, `resume`, `remove`, `seek`, `shuffle`, `loop`, `queue`, `skip`, `np`, `equalizer`, `swap`", color=0xffcff1)
+            embed.set_author(name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}")
+            return await ctx.send(embed=embed)
+        elif command == "image":
+            embed=discord.Embed(title="Image Manipulation Commands", description=f"• `ach`, `sup`, `px`, `pl`, `asc`, `agl`, `pnt`, `clr`, `trl`, `st`, `wst`, `jl`, `chc`, `rnbw`, `wide`, `round`, `stk`, `amg`", color = 0xffcff1)
+            embed.set_author(name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}")
+            return await ctx.send(embed=embed)
+        elif command == "fun":
+            embed=discord.Embed(title="Fun commands", description=f"• `lick`, `kiss`, `kill`, `slap`, `hug`, `punch`, `pat`, `nitro`, `ship`, `rspeed`, `ph`, `choose`, `time`, `roast`, `count`", color=0xffcff1)
+            embed.set_author(name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}")
+            return await ctx.send(embed=embed)
+        elif command == "utility":
+            embed=discord.Embed(title="Utility commands", description=f"• `mc`, `info`, `invite`, `calc`, `vote`, `ui`, `ggl`, `html`, `avatar`, `searchanime`, `check`, `afk`, `remind`", color = 0xffcff1)
+            embed.set_author(name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}")
+            return await ctx.send(embed=embed)
+        elif command == "reddit":
+            embed=discord.Embed(title="Reddit commands", description=f"• `mizuhara`, `nezuko`, `anime`, `meme`, `sftw`, `prgmh`, `bdb`, `linux`, `cpics`, `fpalm`, `cdesign`, `justwtf`, `pshop`", color = 0xffcff1)
+            embed.set_author(name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}")
+            return await ctx.send(embed=embed)
+        elif command == "nsfw":
+            if ctx.channel.is_nsfw():
+                embed=discord.Embed(title="NSFW commands", description=f"• `neko`, `yuri`, `hentai`, `porn`, `waifu`, `cum`, `amateur`, `thot`, `college`, `porngif`, `phcomm`", color = 0xffcff1)
+                embed.set_author(name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}")
+                return await ctx.send(embed=embed)
+            else:
+                url = str(ctx.author.avatar_url_as(format="png"))
+                img = await dagpi.image_process(ImageFeatures.jail(), url)
+                file = discord.File(fp=img.image,filename=f"jail.{img.format}")
+                await ctx.send("<:redTick:596576672149667840> **HORNY ALERT!!** <:redTick:596576672149667840>")
+                return await ctx.send(file=file)
+        elif command == "welcome":
+            embed=discord.Embed(title="Welcome commands (click here for youtube tutorial)", url="https://youtu.be/dKFYxT3Yn3g", description="`setwelc`, `welcset`, `setmex`, `setroles`, `delroles`, `setassign`, `setchannel`, `emb`, `wel`\n\n**You can also use some variables in the welcome message!**\n```py\n{name} > will return the member name\n{member} > will return like Name#1234\n{mention} > will return the member mention\n{count} > will return the position where it joined\n{age} > will return the account date creation\n{guild} > will return the guild name\n```", color = 0xffcff1)
+            embed.set_author(name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}")
+            return await ctx.send(embed=embed)
+
         if command == None:
             ss = "[Support](https://discord.gg/ZcErEwmVYu)"
             ss2 = "[Donate](https://donatebot.io/checkout/800176902765674496)"
             lat = (round(self.bot.latency*1000, 2))
             em = discord.Embed(description=f"```diff\n--- Commands Menu ---\n- Total commands » {len(self.bot.commands)} -\n! Latency » {lat}ms !\n```", color = 0xffcff1)
-            em.add_field(name="\u200b", value = f"<:1713_rem_neutral_face:819689871009382481> × **Moderation** <- `Moderation commands.`\n<:6730_1_first:819689872267673600> × **Economy** <- `Economy commands.`\n<a:7337_Furry_Dance_Green:819689872923033631> × **Music** <- `Music commands.`\n<:4626_FoxWave:819689871550971905> × **Images** <- `Images commands.`\n<:7280_PandaGrimReaper:819689872117465118> × **Fun** <- `Fun commands.`\n<:1739_CMD:819689870393999380> × **Utility** <- `Utility commands.`\n<:bigbrain:744344773229543495> × **Reddit** <- `Reddit commands.`\n<:pepoS:596577130893279272>  × **NSFW** <- `NSFW commands.`\n<:Hehe:729328041301508146> × **Welcome** <- `Welcome commands`\n\n♪ `ami support` / {ss} <- __Support Server__\n♪ `ami donate` / {ss2} <- __Donations__\n♪ `ami feature` <- __To suggest new commands/features__\n")
+            em.add_field(name="\u200b", value = f"<:1713_rem_neutral_face:819689871009382481> × **Moderation** <- `Moderation commands.`\n<:6730_1_first:819689872267673600> × **Economy** <- `Economy commands.`\n<a:7337_Furry_Dance_Green:819689872923033631> × **Music** <- `Music commands.`\n<:4626_FoxWave:819689871550971905> × **Image** <- `Images commands.`\n<:7280_PandaGrimReaper:819689872117465118> × **Fun** <- `Fun commands.`\n<:1739_CMD:819689870393999380> × **Utility** <- `Utility commands.`\n<:bigbrain:744344773229543495> × **Reddit** <- `Reddit commands.`\n<:pepoS:596577130893279272>  × **NSFW** <- `NSFW commands.`\n<:Hehe:729328041301508146> × **Welcome** <- `Welcome commands.`\n\n♪ `ami support` / {ss} <- __Support Server__\n♪ `ami donate` / {ss2} <- __Donations__\n♪ `ami feature` <- __To suggest new commands/features__\n")
             em.add_field(name="\u200b", value = "<a:7041_loading:819689872599285770> __Click on reaction to see commands in categories__ <a:7041_loading:819689872599285770>", inline = False)
             em.set_footer(text="Type ami help <command> for more info on a specific command!")
             msg = await ctx.send(embed=em)
@@ -46,37 +93,37 @@ class Help(commands.Cog):
                     return await msg.delete()
 
                 elif payload.emoji.name == "1713_rem_neutral_face":
-                    embed=discord.Embed(title="Moderation commands", description=f"• `ban`, `kick`, `mute`, `unban`, `unmute`, `clear`, `slowmode`, `antispam`", color=0xffcff1)
+                    embed=discord.Embed(title="Moderation commands", description=f"• `ban`, `kick`, `mute`, `muterole`, `unban`, `unmute`, `cleanup`, `slowmode`, `antispam`", color=0xffcff1)
                     embed.set_author(name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}")
                     await msg.edit(embed=embed)
 
                 elif payload.emoji.name == "6730_1_first":
-                    embed=discord.Embed(title="Economy commands", description=f"• `bal`, `dep`, `wd`, `shop`, `buyitem`, `garage`, `drink`, `hunt`, `smoke`, `fish`, `bankrob`, `openchest`, `oc`, `rob`, `send`, `daily`, `mine`, `invest`, `type`, `lvl`, `setlevelbg`, `lvlup`, `petshop`, `buypet`, `petname`, `lb`, `glbc`", color=0xffcff1)
+                    embed=discord.Embed(title="Economy commands", description=f"• `bal`, `dep`, `wd`, `shop`, `buyitem`, `garage`, `drink`, `hunt`, `smoke`, `fish`, `bankrob`, `openchest`, `oc`, `rob`, `send`, `daily`, `mine`, `invest`, `petshop`, `buypet`, `petname`, `lb`, `glbc`", color=0xffcff1)
                     embed.set_author(name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}")
                     await msg.edit(embed=embed)
 
                 elif payload.emoji.name == "7337_Furry_Dance_Green":
-                    embed=discord.Embed(title="Music commands", description=f"• `play`, `volume`, `leave`, `pause`, `resume`, `summon`, `shuffle`, `queue`, `remove`, `skip`, `now`", color=0xffcff1)
+                    embed=discord.Embed(title="Music commands", description=f"• `play`, `join`, `leave`, `volume`, `stop`, `pause`, `resume`, `remove`, `seek`, `shuffle`, `loop`, `queue`, `skip`, `np`, `equalizer`, `swap`", color=0xffcff1)
                     embed.set_author(name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}")
                     await msg.edit(embed=embed)
 
                 elif payload.emoji.name == "4626_FoxWave":
-                    embed=discord.Embed(title="Image(s) commands", description=f"• `ach`, `sup`, `px`, `pl`, `asc`, `agl`, `pnt`, `clr`, `trl`, `st`, `wst`, `slr`, `jl`, `chc`, `rnbw`", color = 0xffcff1)
+                    embed=discord.Embed(title="Image Manipulation Commands", description=f"• `ach`, `sup`, `px`, `pl`, `asc`, `agl`, `pnt`, `clr`, `trl`, `st`, `wst`, `jl`, `chc`, `rnbw`, `wide`, `round`, `stk`, `amg`", color = 0xffcff1)
                     embed.set_author(name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}")
                     await msg.edit(embed=embed)
 
                 elif payload.emoji.name == "7280_PandaGrimReaper":
-                    embed=discord.Embed(title="Fun commands", description=f"• `lick`, `kiss`, `kill`, `slap`, `hug`, `punch`, `pat`, `echo`, `nitro`, `ship`, `rspeed`, `ph`, `devquiz`, `choose`, `time`, `emojis`, `roast`, `amg`, `wide`, `stk`", color=0xffcff1)
+                    embed=discord.Embed(title="Fun commands", description=f"• `lick`, `kiss`, `kill`, `slap`, `hug`, `punch`, `pat`, `nitro`, `ship`, `rspeed`, `ph`, `choose`, `time`, `roast`, `count`", color=0xffcff1)
                     embed.set_author(name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}")
                     await msg.edit(embed=embed)
 
                 elif payload.emoji.name == "1739_CMD":
-                    embed=discord.Embed(title="Utility commands", description=f"• `syt`, `mc`, `info`, `emoji`, `covid`, `invite`, `calc`, `vote`, `ui`, `ggl`, `html`, `avatar`, `sa`, `sm`, `check`, `afk`", color = 0xffcff1)
+                    embed=discord.Embed(title="Utility commands", description=f"• `mc`, `info`, `invite`, `calc`, `vote`, `ui`, `ggl`, `html`, `avatar`, `sa`, `check`, `afk`, `remind`", color = 0xffcff1)
                     embed.set_author(name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}")
                     await msg.edit(embed=embed)
 
                 elif payload.emoji.name == "bigbrain":
-                    embed=discord.Embed(title="Subreddits commands", description=f"• `mizuhara`, `nezuko`, `anime`, `meme`, `sftw`, `prgmh`, `bdb`, `linux`, `cpics`, `fpalm`, `cdesign`, `justwtf`, `pshop`", color = 0xffcff1)
+                    embed=discord.Embed(title="Reddit commands", description=f"• `mizuhara`, `nezuko`, `anime`, `meme`, `sftw`, `prgmh`, `bdb`, `linux`, `cpics`, `fpalm`, `cdesign`, `justwtf`, `pshop`", color = 0xffcff1)
                     embed.set_author(name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}")
                     await msg.edit(embed=embed)
 
@@ -86,22 +133,27 @@ class Help(commands.Cog):
                     await msg.edit(embed=embed)
 
                 elif payload.emoji.name == "Hehe":
-                    embed=discord.Embed(title="Welcome commands", description="• **setwelc** > `Set a custom welcome message!` -> (__ami setwelc__)\n• **welcset** > `See welcome settings` -> (__ami welcset__)\n• **setmex** > `Change welcome message` -> (__ami setmex <message>__)\n• **setrolename** > `Change role assigned` -> (__ami setrolename <rolename>__)\n• **setassign** > `Enable or disable role assignment` -> (__ami setassign <on/off>__)\n• **setchannel** > `Change channel for the welcome message` -> (__ami setchannel <channelID>__)\n• **emb** > `Enable or disable embed` -> (__ami emb <on/off>__)\n• **wel** > `Enable or disable welcome message` -> (__ami wel <on/off>__)\n\n**You can also use some variables in the welcome message!**\n```py\n{name} > will return the member name\n{member} > will return like Name#1234\n{mention} > will return the member mention\n{count} > will return the position where it joined\n{age} > will return the accout date creation\n{guild} > will return the guild name\n```", color = 0xffcff1)
+                    embed=discord.Embed(title="Welcome commands (click here for youtube tutorial)", url="https://youtu.be/dKFYxT3Yn3g", description="`setwelc`, `welcset`, `setmex`, `setroles`, `delroles`, `setassign`, `setchannel`, `emb`, `wel`\n\n**You can also use some variables in the welcome message!**\n```py\n{name} > will return the member name\n{member} > will return like Name#1234\n{mention} > will return the member mention\n{count} > will return the position where it joined\n{age} > will return the account date creation\n{guild} > will return the guild name\n```", color = 0xffcff1)
                     embed.set_author(name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}")
                     await msg.edit(embed=embed)
 
         else:
             command = self.bot.get_command(command)
             if command == None:
-                return await ctx.send("Command not found.")
+                return await ctx.send("<:redTick:596576672149667840> Command not found.")
             hel = command.help
             if not hel:
-                hel = "No help for this command."
+                hel = "No help provided for this command."
             
-            em = discord.Embed(title=f"Help for {command.name}", description = f"`{hel}`", color = 0xffcff1)
+            cmd_alias = command.aliases
+            if not cmd_alias:
+                cmd_alias = ["No aliases"]
+
+            m = "".join(cmd_alias)
+            em = discord.Embed(title=f"Help for {command.name}", description = f"{hel}", color = 0xffcff1)
             em.add_field(name='Syntax', value=f"> `ami {command.name} {command.signature}`")
-            em.add_field(name='Aliases', value=f"> `{command.aliases}`")
-            em.set_footer(text="[] = Optional | <> = Required", icon_url=ctx.author.avatar_url)
+            em.add_field(name='Aliases', value=f"> `{m}`")
+            em.set_footer(text="[] = Optional | <> = Required | []... = Required (can be multiple)", icon_url=ctx.author.avatar_url)
             await ctx.send(embed=em)
 
 
