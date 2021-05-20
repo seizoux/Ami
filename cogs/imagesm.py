8f090de79f3e53e66ad9cf9ee7a2b9c22fe2425d
Daishiky <77997662+Daishiky@users.noreply.github.com>
2021-05-21 00:56:16 +0200

    Update imagesm.py

--- cogs/imagesm.py
+++ cogs/imagesm.py
@@ -2,28 +2,71 @@ import alexflipnote
 import asyncio
 import discord
 from discord.ext import commands
-from io import BytesIO
 import vacefron
 from asyncdagpi import Client, ImageFeatures
 import typing
 import aiohttp
-from polaroid import Image
+from polaroid import Image as imdagpi
 import cv2 as cv
 from urllib.request import Request, urlopen
 import numpy as np
+from io import BytesIO
+from PIL import Image ,ImageDraw
+
 
 dagpi = Client("token")
 vac_api = vacefron.Client()
 alex_api = alexflipnote.Client("token")
 
+def round_func(pfp: discord.Member):
+    with Image.open(pfp).convert("RGBA") as pfp_1:
+        im = pfp_1.resize((370,370))
+        bgs = Image.new("RGBA", (0,0), 0)
+        bg = bgs.resize(im.size, Image.ANTIALIAS)
+        bigsize = (im.size[0] * 3, im.size[1] * 3)
+        mask = Image.new('L', bigsize, 0)
+        draw = ImageDraw.Draw(mask) 
+        draw.ellipse((0, 0) + bigsize, fill=255)
+        mask = mask.resize(im.size, Image.ANTIALIAS)
+        im.putalpha(mask)
+        bg.paste(im,(0,0),im)
+        im.close()
+
+    buffer = BytesIO()
+    bg.save(buffer, format="PNG")
+    buffer.seek(0)
+
+    return buffer
+
+
 class Imagesm(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.category = "Image(s)"
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print(f"Flipnote Loaded")
+        print(f"Image(s) Loaded")
 
+    @commands.command(help="Have an image you want to round (circle)? This command is for you. (Works only with member avatars).")
+    async def round(self, ctx, member: typing.Optional[typing.Union[discord.Member, str]]):
+        if member == None:
+            member = ctx.author
+        else:
+            member = member
+
+        try:
+            asset1 = member.avatar_url_as(size=512)
+            pfp= BytesIO(await asset1.read())
+        except Exception:
+            return await ctx.reply("<:redTick:596576672149667840> Unable to round, remember it works only with avatars, so if you want to round an external image, set that as your avatar.")
+
+        buffer = await self.bot.loop.run_in_executor(None, round_func, pfp)
+        file=discord.File(fp=buffer, filename="round.png")
+
+        await ctx.send(f"<:greenTick:596576670815879169> Image rounded in `{round(self.bot.latency*1000, 2)}ms`!", file=file)
+
+            
 
     @commands.command(help="Get an achievement", aliases = ["ach"])
     async def achievment(self, ctx, *,text: str, icon = None):
@@ -41,13 +84,29 @@ class Imagesm(commands.Cog):
         await ctx.send(embed=embed, file=image)
 
     @commands.command(help="Generate an '.. is not the impostor' or '... is the impostor' meme", aliases = ["amg"])
-    async def amongus(self, ctx, name, crewmate, impostor = None):
-        if impostor == "true":
-            impostor = True
+    async def amongus(self, ctx, color, false_or_true, *, text):
+        if len(text) > 14:
+            return await ctx.send("The text can't be over 14 characters.")
+        s = ["True", "true", "False", "false"]
+        if false_or_true in s:
+            if false_or_true == "true":
+                false_or_true = True
+            elif false_or_true == "True":
+                false_or_true = True
+            else:
+                if false_or_true == "false":
+                    false_or_true = False
+                if false_or_true == "False":
+                    false_or_true = False
         else:
-            impostor = False
+            return await ctx.reply("Only `False` & `True` are allowed in `<false_or_true>` argument.")
 
-        image = await vac_api.ejected(name, crewmate, impostor)
+        colors = ["red", "blue", "black", "white", "pink", "purple", "brown", "green", "cyan", "yellow", "orange"]
+        if color not in colors:
+            d = ", ".join(colors)
+            return await ctx.reply(f"This color isn't supported, available colors are:\n{d}", mention_author=False)
+
+        image = await vac_api.ejected(text, color, false_or_true)
         image_out = discord.File(fp = await image.read(), filename = "ejected.png")
         await ctx.send(file = image_out)
 
@@ -283,28 +342,6 @@ class Imagesm(commands.Cog):
         return await ctx.send(embed=embed, file=file)
 
 
-    @commands.command(help="Apply the solarize effect on a member avatar", aliases = ["slr"])
-    async def solarize(self, ctx, member: typing.Optional[typing.Union[discord.Member, str]]):
-        if member == None:
-            url = str(ctx.author.avatar_url_as(format="png"))
-        elif isinstance(member, discord.Member) or isinstance(member, discord.User):
-            url = str(member.avatar_url_as(format="png"))
-        else:
-            if member.startswith("https"):
-                url = member
-                url = member.replace("cdn.discordapp.com", "media.discordapp.net")
-            if member.startswith("http"):
-                url = member
-                url = member.replace("cdn.discordapp.com", "media.discordapp.net")
-            else:
-                return await ctx.send("Image not found.")
-
-        img = await dagpi.image_process(ImageFeatures.solar(), url)
-        file = discord.File(fp=img.image,filename=f"solar.{img.format}")
-        embed = discord.Embed(color=0xffcff1).set_image(url=f"attachment://solar.{img.format}")
-        return await ctx.send(embed=embed, file=file)
-
-
     @commands.command(help="Apply the jail effect on a member avatar", aliases = ["jl"])
     async def jail(self, ctx, member: typing.Optional[typing.Union[discord.Member, str]]):
         if member == None:
@@ -361,30 +398,12 @@ class Imagesm(commands.Cog):
         await ctx.send(f"**{member.name}**, " + roast)
 
     @commands.command(help="Get the rainbow effect with a member avatar.", aliases = ["rnbw"])
-    async def rainbow(self, ctx, member: typing.Optional[typing.Union[discord.Member, str]]):
-        author_id = str(ctx.author.id)
-    
+    async def rainbow(self, ctx, member: typing.Optional[typing.Union[discord.Member, str]] = None):
         if member == None:
-            url = ctx.author
-        elif isinstance(member, discord.Member) or isinstance(member, discord.User):
-            url = member
-        else:
-            if member.startswith('https'):
-                url = member
-                async with aiohttp.ClientSession().get(url) as resp:
-                    av = await resp.read()
-                    im = Image(av)
-                    im.apply_gradient()
-                    buffer = BytesIO(im.save("rnbw.png"))
-                    file = discord.File(buffer, "rnbw.png")
-                    file = discord.File("rnbw.png")
-                    em = discord.Embed(color = 0xffcff1)
-                    em.set_image(url="attachment://rnbw.png")
-                    await ctx.send(embed=em, file=file)
-                    return
-            
-        av = await url.avatar_url_as(format="png").read()
-        im = Image(av)
+            member = ctx.author
+
+        av = await member.avatar_url_as(format="png").read()
+        im = imdagpi(av)
         im.apply_gradient()
         im.save("rnbw.png")
         file = discord.File("rnbw.png")
