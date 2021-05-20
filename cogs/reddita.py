81c97234b9d5bf946375b19e1cef07e2a6af9e41
Daishiky <77997662+Daishiky@users.noreply.github.com>
2021-05-21 01:01:26 +0200

    Update reddita.py

--- cogs/reddita.py
+++ cogs/reddita.py
@@ -5,8 +5,7 @@ import discord
 import TenGiphPy
 import scathach
 
-tokens = {'bot': 'ODAxNzQyOTkxMTg1OTM2Mzg0.YAlHWA.HY0iZ42gMJcMx8VIJeUX2ePMaMc',
-          'tenor': '6T8GAHP2LI7E',}
+tokens = {}
 
 t = TenGiphPy.Tenor(token=tokens['tenor'])
 
@@ -14,17 +13,32 @@ t = TenGiphPy.Tenor(token=tokens['tenor'])
 class Reddita(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.category = "Reddit"
 
     @commands.Cog.listener()
     async def on_ready(self):
         print(f"Reddita Loaded")
 
 
+    @commands.command(help="See Zero Two pics.")
+    async def zerotwo(self, ctx):
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
+
+
+        subreddit = await reddit.subreddit("DarlingInTheFranXX")
+        post = await subreddit.random()
+        em=discord.Embed(title=post.title, url=post.url, color=0xffcff1)
+        em.set_image(url=post.url)
+        em.set_footer(text=f"Posted by {post.author} - {post.score} votes.")
+        await ctx.send(embed=em)
+
     @commands.command(help="See meme images")
     async def meme(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
 
         subreddit = await reddit.subreddit("memes")
@@ -39,9 +53,9 @@ class Reddita(commands.Cog):
     @commands.command(help="See hentai images")
     @commands.is_nsfw()
     async def hentai(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
 
         subreddit = await reddit.subreddit("hentai")
@@ -54,9 +68,9 @@ class Reddita(commands.Cog):
 
     @commands.command(help="See anime images")
     async def anime(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
 
         subreddit = await reddit.subreddit("Melanime")
@@ -70,9 +84,9 @@ class Reddita(commands.Cog):
     @commands.command(help="See porn images")
     @commands.is_nsfw()
     async def porn(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
 
         subreddit = await reddit.subreddit("nsfw")
@@ -86,9 +100,9 @@ class Reddita(commands.Cog):
     @commands.command(help="See waifu images")
     @commands.is_nsfw()
     async def waifu(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
  
         subreddit = await reddit.subreddit("ElfWaifu")
@@ -101,9 +115,9 @@ class Reddita(commands.Cog):
 
     @commands.command(help="See software meme images")
     async def sftw(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
         subreddit = await reddit.subreddit("softwaregore")
         post = await subreddit.random()
@@ -114,9 +128,9 @@ class Reddita(commands.Cog):
 
     @commands.command(help="See programming humor images")
     async def prgmh(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
         subreddit = await reddit.subreddit("ProgrammerHumor")
         post = await subreddit.random()
@@ -128,9 +142,9 @@ class Reddita(commands.Cog):
 
     @commands.command(help="See bad discord bots images")
     async def bdb(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
         subreddit = await reddit.subreddit("BadDiscordBots")
         post = await subreddit.random()
@@ -142,9 +156,9 @@ class Reddita(commands.Cog):
 
     @commands.command(help="See nezuko (demon slayer) images")
     async def nezuko(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
         subreddit = await reddit.subreddit("Nezuko")
         post = await subreddit.random()
@@ -156,9 +170,9 @@ class Reddita(commands.Cog):
 
     @commands.command(help="See linux images")
     async def linux(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
         subreddit = await reddit.subreddit("unixporn")
         post = await subreddit.random()
@@ -182,9 +196,9 @@ class Reddita(commands.Cog):
     @commands.command(help="See amateur images")
     @commands.is_nsfw()
     async def amateur(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
         subreddit = await reddit.subreddit("Amateur")
         post = await subreddit.random()
@@ -197,9 +211,9 @@ class Reddita(commands.Cog):
     @commands.command(help="See college images")
     @commands.is_nsfw()
     async def college(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
         subreddit = await reddit.subreddit("collegesluts")
         post = await subreddit.random()
@@ -212,9 +226,9 @@ class Reddita(commands.Cog):
     @commands.command(help="See pornhub fun comments images")
     @commands.is_nsfw()
     async def phcomm(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
         subreddit = await reddit.subreddit("PornhubComments")
         post = await subreddit.random()
@@ -235,9 +249,9 @@ class Reddita(commands.Cog):
 
     @commands.command(help="See cute images")
     async def cpics(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
         subreddit = await reddit.subreddit("aww")
         post = await subreddit.random()
@@ -249,9 +263,9 @@ class Reddita(commands.Cog):
 
     @commands.command(help="See facepalm images")
     async def fpalm(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
         subreddit = await reddit.subreddit("Facepalm")
         post = await subreddit.random()
@@ -263,9 +277,9 @@ class Reddita(commands.Cog):
 
     @commands.command(help="See weird design images")
     async def cdesign(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
         subreddit = await reddit.subreddit("CrappyDesign")
         post = await subreddit.random()
@@ -277,9 +291,9 @@ class Reddita(commands.Cog):
 
     @commands.command(help="See 'WTF' images")
     async def justwtf(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
         subreddit = await reddit.subreddit("hmmm")
         post = await subreddit.random()
@@ -291,9 +305,9 @@ class Reddita(commands.Cog):
 
     @commands.command(help="See photoshop images")
     async def pshop(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
         subreddit = await reddit.subreddit("PhotoshopBattles")
         post = await subreddit.random()
@@ -305,9 +319,9 @@ class Reddita(commands.Cog):
 
     @commands.command(help="See mizuhara images")
     async def mizuhara(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
         subreddit = await reddit.subreddit("ChizuruMizuhara")
         post = await subreddit.random()
@@ -320,9 +334,9 @@ class Reddita(commands.Cog):
     @commands.command(help="See catgirls images")
     @commands.is_nsfw()
     async def neko(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
         subreddit = await reddit.subreddit("Nekomimi")
         post = await subreddit.random()
@@ -335,9 +349,9 @@ class Reddita(commands.Cog):
     @commands.command(help="See yuri's (girl x girl) images")
     @commands.is_nsfw()
     async def yuri(self, ctx):
-        reddit = asyncpraw.Reddit(client_id='4gaG7w5rEn-xsQ',
-                        client_secret='D6FaeCzwJpzrHj_GwOx0e9TI5kLyhQ',
-                        user_agent='AmiPraw')
+        reddit = asyncpraw.Reddit(client_id='x',
+                        client_secret='x',
+                        user_agent='x')
 
         subreddit = await reddit.subreddit("yuri")
         post = await subreddit.random()
