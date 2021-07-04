2a86364433fe9df744d7ff5b61837e5cbf355fb2
Daishiky <77997662+Daishiky@users.noreply.github.com>
2021-07-04 17:38:21 +0200

    moved using bot.session intead new aiohttp

--- cogs/topgg.py
+++ cogs/topgg.py
@@ -6,8 +6,8 @@ import aiohttp
 class Top(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.dbl_token = 'token'  # set this to your bot's Top.gg token
-        self.bot.dblpy = dbl.DBLClient(self.bot, self.dbl_token, autopost=True, webhook_path='path', webhook_auth="pass", webhook_port=port)
+        self.dbl_token = '[omitted]'  # set this to your bot's Top.gg token
+        self.bot.dblpy = dbl.DBLClient(self.bot, self.dbl_token, autopost=True, webhook_path='/dblwebhook', webhook_auth="gamingdaishiky111$$$", webhook_port=4444)
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -17,13 +17,11 @@ class Top(commands.Cog):
     @tasks.loop(minutes=30)
     async def post(self):
         await self.bot.wait_until_ready()
-        auth = {"Authorization": "token"}
-        async with aiohttp.ClientSession(headers=auth) as session:
-            async with session.get('https://top.gg/api/bots/801742991185936384/stats') as resp:
-                myobj = {'server_count': f'{len(self.bot.guilds)}'}
-                await session.post('https://top.gg/api/bots/801742991185936384/stats', data=myobj)
+        auth = {"Authorization": "[dbl_token]"}
+        async with self.bot.session(headers=auth) as session:
+            myobj = {'server_count': f'{len(self.bot.guilds)}'}
+            await session.post('https://top.gg/api/bots/[bot_id]/stats', data=myobj)
 
-        await session.close()
 
     @commands.Cog.listener()
     async def on_dbl_vote(self, data):
@@ -33,14 +31,18 @@ class Top(commands.Cog):
         user_id = str(data['user'])
         message = ""
         coins = await self.bot.pg_con.fetchrow("SELECT * FROM users WHERE user_id = $1", user_id)
+        guild = self.bot.get_guild(800176902765674496)
 
         if not coins:
             message = f"<:upvote:596577438461591562> **{user}** has upvoted for me!"
         else:
-            message = f"<:upvote:596577438461591562> **{user}** has upvoted for me! <:Girl_wave:813865088997916682> **`+20k Coins`**!"
+            message = f"<:upvote:596577438461591562> **{user}** has upvoted for me! **<:cupcake:845632403405012992> `+20.000`**!"
             await self.bot.pg_con.execute("UPDATE users SET bank = $1 WHERE user_id = $2", coins["bank"] + 20000, user_id)
 
         await d.send(message)
+        if user in guild.members:
+            role = discord.utils.get(guild.roles, id=860981845470478337)
+            await user.add_roles(role)
 
     @commands.Cog.listener()
     async def on_dbl_test(self, data):
@@ -54,7 +56,7 @@ class Top(commands.Cog):
         if not coins:
             message = f"<:upvote:596577438461591562> **{user}** has upvoted for me!"
         else:
-            message = f"<:upvote:596577438461591562> **{user}** has upvoted for me! <:1192_purple_pink_gift:831410660088217600> **`+20k Coins`**!"
+            message = f"<:upvote:596577438461591562> **{user}** has upvoted for me! **<:cupcake:845632403405012992> `+100.000`**!"
             await self.bot.pg_con.execute("UPDATE users SET bank = $1 WHERE user_id = $2", coins["bank"] + 20000, user_id)
 
         await d.send(message)
