c4c475b0e0d49e14bd569b46914e62e304717218
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-18 11:39:20 -0700

    Listeners

--- cogs/ipc.py
+++ cogs/ipc.py
@@ -1,8 +1,20 @@
+import sys
+
 from discord.ext import commands, ipc
 
 class IpcRoutes(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_ipc_ready(self):
+        """Called upon the IPC Server being ready"""
+        print("Ipc is ready.")
+
+    @commands.Cog.listener()
+    async def on_ipc_error(self, endpoint, error):
+        """Called upon an error being raised within an IPC route"""
+        print(endpoint, "raised", error, file=sys.stderr)
     
     @ipc.server.route()
     async def get_guilds_count(self) -> int:
