60b6690dc6616410ad0c87d7058d7c9e2ac6e927
Daishiky <77997662+Daishiky@users.noreply.github.com>
2021-11-07 17:10:08 +0100

    websocket

--- cogs/websocket.py
+++ cogs/websocket.py
@@ -10,7 +10,11 @@ class Websocket(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
-        self.bot.loop.create_task(self.connect_ws())
+        if hasattr(bot, 'web_ws'):
+            if bot.web_ws.closed:
+                self.bot.loop.create_task(self.connect_ws())
+        else:
+            self.bot.loop.create_task(self.connect_ws())
 
     async def connect_ws(self):
         """
@@ -18,25 +22,27 @@ class Websocket(commands.Cog):
         the while loop to keep the connection alive as long as
         you don't stop it
         """
-        if not hasattr(self.bot, "web_ws"):
-            self.bot.web_ws = await self.bot.session.ws_connect('wss://amibot.gg/dashboard/ws/playing')
-            log.info("Websocket Connected")
 
-        while True:
+        self.bot.web_ws = await self.bot.session.ws_connect('wss://amibot.gg/dashboard/ws/playing')
+        log.info("Websocket Connected")
+
+        while self.bot.web_ws.closed != True:
+            log.info("Started receiving...")
             msg = await self.bot.web_ws.receive()
 
-            if msg.tp == aiohttp.MsgType.text:
+            log.info(f"Received {msg}, going next..")
+
+            if msg.type == aiohttp.WSMsgType.TEXT:
                 if msg.data == 'close':
                     await self.bot.web_ws.close()
                     log.error('Websocket closed by msg.data closed text')
                     break
                 else:
-                    await self.send_and_retreive(msg)
-                    log.info("Invoked send_and_receive for websocket")
-            elif msg.tp == aiohttp.MsgType.closed:
-                log.erro("Websocket closed")
+                    log.info(f"Websocket received: {msg}")
+            elif msg.type == aiohttp.WSMsgType.CLOSED:
+                log.error("Websocket closed")
                 break
-            elif msg.tp == aiohttp.MsgType.error:
+            elif msg.type == aiohttp.WSMsgType.ERROR:
                 log.error("Websocket errored.")
                 break
 
