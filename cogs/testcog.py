ed10c53228dac99f221669f83c4edee805eb972e
Jadon <47833807+WhoTheOOF@users.noreply.github.com>
2022-03-14 06:37:59 -0500

    Add new "IpcTest" admin stuff cog

---
+++ cogs/testcog.py
@@ -0,0 +1,59 @@
+import asyncio
+from discord.ext import commands
+
+from util.subclasses import Ami
+
+
+class IpcTest(commands.Cog):
+    def __init__(self, bot: Ami) -> None:
+        self.bot = bot
+        super().__init__()
+
+    @commands.command()
+    @commands.is_owner()
+    async def post(self, ctx: commands.Context) -> None:
+        await self.bot.websocket.send_json(
+            {"cmd": "broadcast", "data": {"payload": {"cmd": "eval", "data": {"payload": 'return 1'}}}}
+        )
+        self.bot.ws_waiter = asyncio.Future()
+        js = await self.bot.ws_waiter
+        await ctx.send(js)
+
+    @commands.command()
+    @commands.is_owner()
+    async def unload(self, ctx: commands.Context, cog: str) -> None:
+        await self.bot.websocket.send_json(
+            {"cmd": "broadcast", "data": {"payload": {"cmd": "unload", "extension": cog}}}
+        )
+        self.bot.ws_waiter = asyncio.Future()
+        js = await self.bot.ws_waiter
+        await ctx.send("Cog has been unloaded on all clusters")
+
+    @commands.command()
+    @commands.is_owner()
+    async def load(self, ctx: commands.Context, cog: str) -> None:
+        await self.bot.websocket.send_json(
+            {"cmd": "broadcast", "data": {"payload": {"cmd": "load", "extension": cog}}}
+        )
+        self.bot.ws_waiter = asyncio.Future()
+        js = await self.bot.ws_waiter
+        await ctx.send("Cog has been loaded on all clusters")
+
+    @commands.command()
+    @commands.is_owner()
+    async def reload(self, ctx: commands.Context, cog: str) -> None:
+        await self.bot.websocket.send_json(
+            {"cmd": "broadcast", "data": {"payload": {"cmd": "unload", "extension": cog}}}
+        )
+        self.bot.ws_waiter = asyncio.Future()
+        js = await self.bot.ws_waiter
+        await self.bot.websocket.send_json(
+            {"cmd": "broadcast", "data": {"payload": {"cmd": "load", "extension": cog}}}
+        )
+        self.bot.ws_waiter = asyncio.Future()
+        js = await self.bot.ws_waiter
+        await ctx.send("Reloaded!")
+
+
+def setup(bot: Ami) -> None:
+    bot.add_cog(IpcTest(bot))
